a5b50e77aa266101e65e219866051e585f4510e3
abdul <125051619+laziz07@users.noreply.github.com>
2023-05-23 18:29:11 +0900

    hp

---
+++ planet/Assets/01.Scripts/RocketHP.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Spine.Unity;
+using UnityEngine.SceneManagement;
+
+public class RocketHP : MonoBehaviour
+{
+    [SerializeField]
+    public int currentHP = 3;     // 최대 체력
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void Awake()
+    {
+        
+    }
+    private void destroy()
+    {
+        Destroy(this.gameObject);
+        SceneManager.LoadScene("Gameover");
+    }
+
+    public void TakeDamage(int damage)
+    {
+        // 현재 체력을 damage만큼 감소
+        currentHP -= damage;
+        Debug.Log("RocketHP : " + currentHP);
+
+        if (currentHP == 0)
+        {
+            Invoke("destroy", 0.5f); // 2초 후에 실행할 액션 지정
+        }
+    }
+}
